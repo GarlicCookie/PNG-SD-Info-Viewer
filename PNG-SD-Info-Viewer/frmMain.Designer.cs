6f04bc9a902872e4f960a671477c680426b5ac33
GarlicCookie <GarlicCookie@local>
2023-02-10 17:16:33 -0500

    Significant updates.  Added image preview mode, size selection, refined multiple other things, cleaned up code, added comments, optimized performance.

--- PNG-SD-Info-Viewer/frmMain.Designer.cs
+++ PNG-SD-Info-Viewer/frmMain.Designer.cs
@@ -38,7 +38,23 @@
             this.lblParameters = new System.Windows.Forms.Label();
             this.lblFolderSelected = new System.Windows.Forms.Label();
             this.btnCopy = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageListWidthToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pxToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pxToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.lblFilename = new System.Windows.Forms.Label();
+            this.quitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.picbImageDisplay)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
             this.SuspendLayout();
             // 
             // picbImageDisplay
@@ -55,7 +71,7 @@
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 26);
             this.btnSelectFolder.Name = "btnSelectFolder";
             this.btnSelectFolder.Size = new System.Drawing.Size(147, 23);
             this.btnSelectFolder.TabIndex = 1;
@@ -118,10 +134,11 @@
             // lblFolderSelected
             // 
             this.lblFolderSelected.AutoSize = true;
-            this.lblFolderSelected.Location = new System.Drawing.Point(165, 20);
+            this.lblFolderSelected.Location = new System.Drawing.Point(165, 30);
             this.lblFolderSelected.Name = "lblFolderSelected";
-            this.lblFolderSelected.Size = new System.Drawing.Size(0, 15);
+            this.lblFolderSelected.Size = new System.Drawing.Size(97, 15);
             this.lblFolderSelected.TabIndex = 7;
+            this.lblFolderSelected.Text = "lblFolderSelected";
             // 
             // btnCopy
             // 
@@ -133,11 +150,144 @@
             this.btnCopy.UseVisualStyleBackColor = true;
             this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.menuToolStripMenuItem,
+            this.sizeToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1225, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "txt";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.quitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileListToolStripMenuItem,
+            this.imageListToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.menuToolStripMenuItem.Text = "List";
+            // 
+            // fileListToolStripMenuItem
+            // 
+            this.fileListToolStripMenuItem.Name = "fileListToolStripMenuItem";
+            this.fileListToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fileListToolStripMenuItem.Text = "File List";
+            this.fileListToolStripMenuItem.Click += new System.EventHandler(this.fileListToolStripMenuItem_Click);
+            // 
+            // imageListToolStripMenuItem
+            // 
+            this.imageListToolStripMenuItem.Name = "imageListToolStripMenuItem";
+            this.imageListToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.imageListToolStripMenuItem.Text = "Image List";
+            this.imageListToolStripMenuItem.Click += new System.EventHandler(this.imageListToolStripMenuItem_Click);
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imageListWidthToolStripMenuItem});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.sizeToolStripMenuItem.Text = "Size";
+            // 
+            // imageListWidthToolStripMenuItem
+            // 
+            this.imageListWidthToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pxToolStripMenuItem,
+            this.pxToolStripMenuItem1,
+            this.pxToolStripMenuItem2});
+            this.imageListWidthToolStripMenuItem.Name = "imageListWidthToolStripMenuItem";
+            this.imageListWidthToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.imageListWidthToolStripMenuItem.Text = "Image List Width";
+            // 
+            // pxToolStripMenuItem
+            // 
+            this.pxToolStripMenuItem.Name = "pxToolStripMenuItem";
+            this.pxToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.pxToolStripMenuItem.Text = "64px";
+            this.pxToolStripMenuItem.Click += new System.EventHandler(this.pxToolStripMenuItem_Click);
+            // 
+            // pxToolStripMenuItem1
+            // 
+            this.pxToolStripMenuItem1.Name = "pxToolStripMenuItem1";
+            this.pxToolStripMenuItem1.Size = new System.Drawing.Size(105, 22);
+            this.pxToolStripMenuItem1.Text = "128px";
+            this.pxToolStripMenuItem1.Click += new System.EventHandler(this.pxToolStripMenuItem1_Click);
+            // 
+            // pxToolStripMenuItem2
+            // 
+            this.pxToolStripMenuItem2.Name = "pxToolStripMenuItem2";
+            this.pxToolStripMenuItem2.Size = new System.Drawing.Size(105, 22);
+            this.pxToolStripMenuItem2.Text = "256px";
+            this.pxToolStripMenuItem2.Click += new System.EventHandler(this.pxToolStripMenuItem2_Click);
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToAddRows = false;
+            this.dgvMain.AllowUserToDeleteRows = false;
+            this.dgvMain.AllowUserToResizeColumns = false;
+            this.dgvMain.AllowUserToResizeRows = false;
+            this.dgvMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dgvMain.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 70);
+            this.dgvMain.MultiSelect = false;
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.ReadOnly = true;
+            this.dgvMain.RowHeadersVisible = false;
+            this.dgvMain.RowTemplate.Height = 50;
+            this.dgvMain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMain.Size = new System.Drawing.Size(343, 604);
+            this.dgvMain.TabIndex = 11;
+            this.dgvMain.Visible = false;
+            this.dgvMain.SelectionChanged += new System.EventHandler(this.dgvMain_SelectionChanged);
+            // 
+            // lblFilename
+            // 
+            this.lblFilename.AutoSize = true;
+            this.lblFilename.Location = new System.Drawing.Point(410, 52);
+            this.lblFilename.Name = "lblFilename";
+            this.lblFilename.Size = new System.Drawing.Size(68, 15);
+            this.lblFilename.TabIndex = 12;
+            this.lblFilename.Text = "lblFilename";
+            // 
+            // quitToolStripMenuItem1
+            // 
+            this.quitToolStripMenuItem1.Name = "quitToolStripMenuItem1";
+            this.quitToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem1.Text = "Quit";
+            this.quitToolStripMenuItem1.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1225, 687);
+            this.Controls.Add(this.lblFilename);
+            this.Controls.Add(this.dgvMain);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.lblFolderSelected);
             this.Controls.Add(this.lblParameters);
@@ -147,10 +297,15 @@
             this.Controls.Add(this.lstbFilelist);
             this.Controls.Add(this.btnSelectFolder);
             this.Controls.Add(this.picbImageDisplay);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMain";
-            this.Text = "PNG-SD-Info-Viewer, for Stable Diffusion Generated Images - By Omnia";
+            this.Text = "PNG-SD-Info-Viewer, for Stable Diffusion Generated Images";
             ((System.ComponentModel.ISupportInitialize)(this.picbImageDisplay)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,5 +322,19 @@
         private Label lblParameters;
         private Label lblFolderSelected;
         private Button btnCopy;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem fileListToolStripMenuItem;
+        private ToolStripMenuItem imageListToolStripMenuItem;
+        private DataGridView dgvMain;
+        private ToolStripMenuItem sizeToolStripMenuItem;
+        private ToolStripMenuItem imageListWidthToolStripMenuItem;
+        private ToolStripMenuItem pxToolStripMenuItem;
+        private ToolStripMenuItem pxToolStripMenuItem1;
+        private ToolStripMenuItem pxToolStripMenuItem2;
+        private Label lblFilename;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem1;
     }
 }
--- PNG-SD-Info-Viewer/frmMain.cs
+++ PNG-SD-Info-Viewer/frmMain.cs
@@ -1,109 +1,226 @@
 using MetadataExtractor;
 using MetadataExtractor.Formats.FileSystem;
+using System.Data.Common;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace PNG_SD_Info_Viewer
 {
     public partial class frmMain : Form
     {
-
-        List<string> Imagefiles = new List<string>();
+        // ImageList object, used to fill in a DataGridView for image list mode
+        ImageList imgList = new ImageList();
         
+        // A string to hold the open path
+        string openPath = "";
         
-        string openPath = "test";
+        // Default size of the image previews in image list mode
+        int wSize = 128;
+        int hSize = 128;
 
 
-
+        // Launchpoint
         public frmMain()
         {
             InitializeComponent();
-            
-        }
 
-        private void findImagesInDirectory(string path)
-        {
-            
-
-            // Get all the png files from the provided path and put into array
-            
-            DirectoryInfo dinfo = new DirectoryInfo(path);
-            FileInfo[] Files = dinfo.GetFiles("*.png");
-            
-            // Populate the listbox
-            foreach (FileInfo file in Files)
-            {
-                lstbFilelist.Items.Add(file.Name);
-            }
-
-            /*foreach (string s in files)
-            {
-                if (s.EndsWith(".jpg") || s.EndsWith(".png")) //add more format files here
-                {
-                    Imagefiles.Add(s);
-                }
-            }
-
-
-            try
-            {
-                pictureBox1.ImageLocation = Imagefiles.First();
-            }
-            catch { MessageBox.Show("No files found!"); }*/
-
+            // Empty some of the textboxes.
+            lblFolderSelected.Text = "";
+            lblFilename.Text = "";
         }
 
 
-
+        // Method when folder button is selected.  Gathers the path to use and passes it along to the find method.
         private void btnSelectFolder_Click(object sender, EventArgs e)
         {
-
-            // Grab the folder
+            // Grab the folder from a dialog box
             var fbd = new FolderBrowserDialog();
             DialogResult result = fbd.ShowDialog();
             if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
             {
+                // TODO - do I need this?  Got a new folder so clear the stuffs
+                //lstbFilelist.Items.Clear();
+                //txtParameters.Text = "";
+                //picbImageDisplay.Image = null;
 
-                // Got a new folder so clear the stuffs
-                // Clear the stuffs
-                lstbFilelist.Items.Clear();
-                txtParameters.Text = "";
-                picbImageDisplay.Image = null;
-
-
-                // Store the path selected
+                // Store the path selected, it comes in handy.
                 openPath = fbd.SelectedPath;
+
+                // Show the selected path in the UI
                 lblFolderSelected.Text = openPath;
 
-                // Pull up the image list in the folder
+                // Go find images!
                 findImagesInDirectory(fbd.SelectedPath);
             }
         }
 
+
+        // Comb the selected folder and pull images into the ListBox and DataGridView objects
+        private void findImagesInDirectory(string path)
+        {
+            // Reset lists and parameters box
+            lstbFilelist.Items.Clear();
+            dgvMain.DataSource = null;
+            dgvMain.Columns.Clear();
+            dgvMain.Rows.Clear();
+            imgList.Images.Clear();
+            txtParameters.Text = "";
+            picbImageDisplay.Image = null;
+
+            // Set the side of the images in the imagelist
+            imgList.ImageSize = new Size(wSize, hSize);
+
+            // Setup datagridview columns if DGV is active
+            if (dgvMain.Visible == true)
+            {
+                DataGridViewImageColumn iconColumn = new DataGridViewImageColumn();
+                DataGridViewTextBoxColumn txtColumn = new DataGridViewTextBoxColumn();
+                dgvMain.Columns.Add(iconColumn);
+                dgvMain.Columns.Add(txtColumn);
+                iconColumn.Name = "Images";
+                iconColumn.HeaderText = "Images:";
+                iconColumn.ImageLayout = DataGridViewImageCellLayout.Normal;
+                iconColumn.Description = "Zoomed";
+                txtColumn.Name = "Filename:";
+                txtColumn.HeaderText = "Filename:";
+            }
+
+            // Get all the png files from the provided path and put into array
+            DirectoryInfo dinfo = new DirectoryInfo(path);
+            FileInfo[] Files = dinfo.GetFiles("*.png");
+
+            // Populate items into the Listbox, and the ImageList which we'll use for the DGV, using each file
+            foreach (FileInfo file in Files)
+            {
+                // Add item to listbox
+                lstbFilelist.Items.Add(file.Name);
+
+                // Add filename to imgList object, with the full path as a key.  Might use the keyname later to lookup an image.
+                imgList.Images.Add(file.FullName, Image.FromFile(file.FullName));
+            }
+
+            // Add items to the DGV if it is active
+            if (dgvMain.Visible == true)
+            {
+                for (int j = 0; j < imgList.Images.Count; j++)
+                {
+                    // Add a row and set its value to the image in first column, and filename in second
+                    dgvMain.Rows.Add();
+                    dgvMain.Rows[j].Cells[0].Value = imgList.Images[j];
+                    dgvMain.Rows[j].Cells[1].Value = Files[j].Name;
+
+                    // Update cellsize for each row
+                    DataGridViewCell cell = dgvMain.Rows[j].Cells[0];
+
+                    // Set the Row height.
+                    dgvMain.Rows[cell.RowIndex].Height = hSize;
+
+                    // Set the Column height.
+                    dgvMain.Columns[0].Width = wSize;
+                }
+                
+                // All items added, now select nothing.  Otherwise, DGV selects first row by default but doesn't update.
+                dgvMain.ClearSelection();
+
+                // Set focus to the active control.  This doesn't work perfectly but it's ok.
+                if (dgvMain.Visible == true)
+                {
+                    dgvMain.Focus();
+                }
+                else
+                {
+                    lstbFilelist.Focus();
+                }
+            }
+        }
+
+
+        // Call when a new item is selected in our ListBox
         private void lstbFilelist_SelectedIndexChanged(object sender, EventArgs e)
         {
             // Clear the stuffs
             txtParameters.Text = "";
-
-
+            
+            // Get the filename selected in the listbox
             string selectedImage = lstbFilelist.GetItemText(lstbFilelist.SelectedItem);
 
+            // Update our picturebox and parameters boxes!
+            updateBoxes(selectedImage);
+        }
+
+        // Call this when a new row is selected in our DataGridView
+        private void dgvMain_SelectionChanged(object sender, EventArgs e)
+        {
+            // Clear the stuffs
+            txtParameters.Text = "";
+
+            // Set a string for the selected image filename
+            string selectedImage = "";
+
+            // Pull out any selected rows. There should only be one since we limit it to single row selection.  TODO: Replace the foreach, we don't need to iterate.
+            foreach (DataGridViewRow selectedRow in dgvMain.SelectedRows)
+            {
+                // If the value in the second column isn't null (and it shouldn't be, it should have the filename text), then store the filename as a string.
+                if (dgvMain[1, selectedRow.Index].Value != null)
+                {
+                    selectedImage = dgvMain[1, selectedRow.Index].Value.ToString()!;
+                }
+            }
+
+            // Update our picturebox and parameters boxes!
+            updateBoxes(selectedImage);
+        }
+
+
+        // Call this when a new row is selected in our DataGridView
+        private void dgvMain_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Clear the stuffs
+            txtParameters.Text = "";
+
+            // Set a string for the selected image filename
+            string selectedImage = "";
+            
+            // Pull out any selected rows. There should only be one since we limit it to single row selection.  TODO: Replace the foreach, we don't need to iterate.
+            foreach (DataGridViewRow selectedRow in dgvMain.SelectedRows)
+            {
+                // If the value in the second column isn't null (and it shouldn't be, it should have the filename text), then store the filename as a string.
+                if (dgvMain[1, selectedRow.Index].Value.ToString() != null)
+                {
+                    selectedImage = dgvMain[1, selectedRow.Index].Value.ToString()!;
+                }
+            }
+
+            // Update our picturebox and parameters boxes!
+            updateBoxes(selectedImage);
+        }
+
+        private void updateBoxes(string selectedImage)
+        {
+            // Ensure we have an image to use
+            if (selectedImage == "")
+            {
+                return;
+            }
+            
+            // Set the picturebox to display the image based on the stored path plus the selected filename
             picbImageDisplay.ImageLocation = openPath + "\\" + selectedImage;
 
+            // Show the selected filename on the UI
+            lblFilename.Text = selectedImage;
 
-
-            // EXIF
+            // Grab the EXIF data and show it in the parameters textbox
             IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(openPath + "\\" + selectedImage);
-
-
             foreach (var directory in directories)
                 foreach (var tag in directory.Tags)
                 {
                     if (tag.Name == "Textual Data")
                     {
+                        // Old style of EXIF pull.  Left for reference.
                         //textBox1.Text += ($"{directory.Name} \r\n - {tag.Name} = {tag.Description}");
                         //textBox1.Text += "\n";
 
@@ -117,16 +234,108 @@ namespace PNG_SD_Info_Viewer
                         parsed = parsed.Replace("Model:", "\r\nModel:");
                         parsed = parsed.Replace("Denoising strength:", "\r\nDenoising strength:");
 
-                        //txtParameters.Text += ($"{tag.Description}");
+                        // Update the textbox with the parsed string
                         txtParameters.Text += parsed;
                     }
                 }
-
         }
 
+        
+        // Simple method to copy parameters to clipboard
         private void btnCopy_Click(object sender, EventArgs e)
         {
+            // Copies to clipboard
             Clipboard.SetText(txtParameters.Text);
         }
+
+
+        // Method to show listbox and populate it with the selected folder if there is one
+        private void fileListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Hide DataGridView and Show Listbox
+            dgvMain.Hide();
+            lstbFilelist.Show();
+            
+            // Populate the Listbox if a path is selected
+            if (openPath != "")
+            {
+                findImagesInDirectory(openPath);
+            }
+        }
+
+
+        // Method to show DataGridView and populate it with the selected folder if there is one
+        private void imageListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Hide Listbox and Show DataGridview
+            dgvMain.Show();
+            lstbFilelist.Hide();
+
+            // Populate the DGV if a path is selected
+            if (openPath != "")
+            {
+                findImagesInDirectory(openPath);
+            }
+        }
+
+
+        // Quit button
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Empty memory.  Probably not really necessary, but whatever.
+            imgList.Dispose();
+            lstbFilelist.Items.Clear();
+            dgvMain.DataSource = null;
+            dgvMain.Columns.Clear();
+            dgvMain.Rows.Clear();
+            openPath = "";
+
+            // Close
+            this.Close();
+        }
+
+
+        // About button
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtParameters.Text = "PNG-SD-Info-Viewer is a program designed to quickly allow the browsing of PNG files with associated metadata from Stable Diffusion generated images.\r\n - By Omnia the Garlic Cookie\r\nhttps://github.com/GarlicCookie/PNG-SD-Info-Viewer\r\nGNU GPL3";
+        }
+
+
+        // Set to 64px and go repopulate the list
+        private void pxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hSize = 64;
+            wSize= 64;
+            if (openPath != "")
+            {
+                findImagesInDirectory(openPath);
+            }
+        }
+
+
+        // Set to 128px and go repopulate the list
+        private void pxToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            hSize = 128;
+            wSize = 128;
+            if (openPath != "")
+            {
+                findImagesInDirectory(openPath);
+            }
+        }
+
+
+        // Set to 256px and go repopulate the list
+        private void pxToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            hSize = 256;
+            wSize = 256;
+            if (openPath != "")
+            {
+                findImagesInDirectory(openPath);
+            }
+        }
+
     }
 }
